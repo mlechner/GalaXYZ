3b2f658a58fac5dd0bf4354e0df443db521836c6
Isaac Boates <iboates@gmail.com>
2017-11-10 17:09:13 +0100

    Update config.js

--- web/config/config.js
+++ web/config/config.js
@@ -5,9 +5,9 @@ var configParams = {
 	"db_schema": "public",
 	"db_user": "postgres",
 	"db_pass": "",
-	"geo_srid": 4326
+	"geo_srid": 4326,
 	"geo_bbox": // bottom left x, bottom left y, top right x, top right y
-		8.24389890034,
+		[8.24389890034,
 		48.9420024184,
 		8.55974321228,
 		49.0789748009]
