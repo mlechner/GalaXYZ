c10d0883680252ee8e72f3620b201ee01f477a1c
Isaac Boates <iboates@gmail.com>
2017-11-10 17:04:45 +0100

    first commit

---
+++ web/config/config.js
@@ -0,0 +1,14 @@
+var configParams = {
+	"db_host": "localhost",
+	"db_port": 5432,
+	"db_name": "nogo_dijkstra",
+	"db_schema: "public",
+	"db_user": "postgres",
+	"db_pass": "",
+	"geo_srid": 4326
+	"geo_bbox": // bottom left x, bottom left y, top right x, top right y
+		8.24389890034,
+		48.9420024184,
+		8.55974321228,
+		49.0789748009]
+};
