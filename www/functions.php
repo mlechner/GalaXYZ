602e4c3144bba612c2045468bebabcdaf4122caf
robcoppinger <32789173+robcoppinger@users.noreply.github.com>
2017-11-12 18:31:41 +0100

    functions updated
    Now gets DB connection params from the config file

--- www/functions.php
+++ www/functions.php
@@ -3,13 +3,21 @@
 	ini_set('display_errors', 1); // Dispalys errors
 
 	function dbconn(){
+
+		ini_set('display_errors', 1); // Dispalys errors
+		// fetch config params
+		$config = file_get_contents('config.json');
+		$json = json_decode($config, true);
+		//echo $json['db_host'];
+
+
 		//database login info
-		$host = 'localhost';
-		$port = '5432';
-		$dbname = 'galaxyz';
-		$user = 'postgres';
-		$password = 'GalaXYZ_pwd';
-		// establish connection
+		$host = $json['db_host'];
+		$port = $json['db_port'];
+		$dbname = $json['db_name'];
+		$user = $json['db_user'];
+		$password = $json['db_pwd'];
+		// establish connection 		
 		$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 		if (!$conn) {
 			echo "Not connected : " . pg_error();
