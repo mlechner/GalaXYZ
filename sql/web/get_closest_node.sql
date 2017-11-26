2293f93c2ddb7b82095cb9a16b93b02c0cb5417f
Isaac Boates <iboates@gmail.com>
2017-11-26 15:26:55 +0100

    updated database backup.  reorganized sql folder.  to use the necessary functions on the website, go to folder sql/web/.  necessary function definitions should be already pre-defined when restoring the backup, however if not, go to the sql/def/ folder and execute define_overwrite_cost.sql and define_nogo_Dijkstra.sql in that order.

---
+++ sql/web/get_closest_node.sql
@@ -0,0 +1,17 @@
+﻿/*
+Query to return the id of the closest node in the ways_vertices_pgr table.
+Use this query to get the ids required for routing start/stop locations.
+*/
+
+SELECT
+	id/*,
+	ST_AsGeoJSON(ST_ClosestPoint(ways_vertices_pgr.the_geom, ST_GeomFromEWKT('SRID=4326;POINT(8.402266 49.007979)'))) AS geom*/
+FROM
+	ways_vertices_pgr
+ORDER BY
+	ST_Distance(
+		ways_vertices_pgr.the_geom,
+		ST_GeomFromEWKT('SRID=4326;POINT(8.402266 49.007979)') /* Put user click coordinates here */
+	) ASC
+LIMIT
+	1;
