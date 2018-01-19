d47bc2b63522335c85c65ee52a2e34ddeb524a1f
Rob Coppinger <coppinger.robert@gmail.com>
2018-01-19 15:14:15 +0100

    Secure DB querying - now includes pg_query_params; routes can be clculated without nogo areas

--- www/php/ajax_nogoDijkstra.php
+++ www/php/ajax_nogoDijkstra.php
@@ -1,91 +1,140 @@
 <?php
-	
+
+    
 	//includes the functions from 'functions.php' for the database connection
 	require("functions.php");
 	//creates the connection
 	$conn = dbconn();
 
 	$geoms1 = $_POST['data']; //geometry 'POST' data passed from ajax
-	$nodes1 = $_POST['nodes']; //node id of the 'from' location, passed from ajax
-	//$to = $_POST['to']; //node id of the 'to' location, passed from ajax
+	$nodes = json_decode($_POST['nodes']); //node id of the 'from' location, passed from ajax
 
 	$geoms = json_decode($geoms1, true); //makes the geoms1 a php array
-	$nodes = json_decode($nodes1, true);
 
-	$FeatureCollection = ""; //declare the feature collection variable
-
-	$count = 0;
-	//for each geometry (NoGo polygon):
-	for ($x = 0; $x < sizeof($geoms); $x++){
-		//remove the leaflet id from the array
-		unset($geoms[$x]["id"]);
-		//for every geometry except the first one
-		if ($count > 0){
-			//prepend a comma separator to separate the features
-			$FeatureCollection.= ", ";
-		}
-		//add the geometry to the feature collection
-		$FeatureCollection.= json_encode($geoms[$x]);
-		$count += 1;
-	}
-
-	
-	//sql function calling the nogo_Dijkstra function from the DB
-	$sql = 'SELECT json_build_object(
-			\'type\',		\'FeatureCollection\',
-			\'features\', 	jsonb_agg(feature)
-		)
-		FROM (
-			SELECT json_build_object(
-				\'type\',		\'Feature\',
-				\'id\', 		gid,
-				\'geometry\',	ST_AsGeoJSON(geom)::json,
-				\'properties\',	to_jsonb(row) - \'id\' - \'geom\'
-		) AS
-			feature
-		FROM
-			(
-				SELECT
-					seq AS gid,
-					route_result.id1 AS route_id,
-					route_result.id3 AS edge_id,
-					ways.name,
-					the_geom AS geom
-				FROM pgr_nogo_trspViaVertices(
-						\'SELECT gid::INTEGER AS id, source::INTEGER, target::INTEGER, cost::FLOAT8, reverse_cost::FLOAT8, the_geom AS geom FROM ways\',
-						(
-							SELECT
-								ST_SetSRID(ST_Union(ST_GeomFromGeoJSON(feat->>\'geometry\')), 4326)
-							FROM (
-								SELECT json_array_elements(
-									\'{
-										"type": "FeatureCollection",
-										"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-										"features":
-											['.$FeatureCollection.']
-									}\'
-									::json->\'features\'
-								) AS
-									feat
-							) AS
-								f
-						),
-						ARRAY'.json_encode($nodes).',
-						TRUE,
-						TRUE
-					) AS route_result
-				INNER JOIN
-					ways
-				ON
-					ways.gid = route_result.id3
+	// if no nogo area has been set
+	if (empty($geoms)){
+		// run the normal pgr function
+		$sql = "SELECT json_build_object(
+				'type',		'FeatureCollection',
+				'features', 	jsonb_agg(feature)
 			)
-		row) features;
+			FROM (
+				SELECT json_build_object(
+					'type',		'Feature',
+					'id', 		gid,
+					'geometry',	ST_AsGeoJSON(geom)::json,
+					'properties',	to_jsonb(row) - 'id' - 'geom'
+			) AS
+				feature
+			FROM
+				(
+					SELECT
+						seq AS gid,
+						route_result.id1 AS route_id,
+						route_result.id3 AS edge_id,
+						ways.name,
+						the_geom AS geom
+					FROM pgr_trspViaVertices(
+							'SELECT gid::INTEGER AS id, source::INTEGER, target::INTEGER, cost::FLOAT8, reverse_cost::FLOAT8 FROM ways',
+							$1::INTEGER[],
+							TRUE,
+							TRUE
+						) AS route_result
+					INNER JOIN
+						ways
+					ON
+						ways.gid = route_result.id3
+				)
+			row) features;";
+
+		//query the DB and pass necessary params
+		$result = pg_query_params( $conn, $sql, array(to_pg_array($nodes)) ) or die('Query Failed: ' .pg_last_error());
+
+		$finalvalue = pg_fetch_result($result, 0, 0);
+	    echo $finalvalue; //send result back to web page*/
+
+	} else {
+		// Nogo areas have been specified
+		// run the nogo sql function
+		$FeatureCollection = ""; //declare the feature collection variable
+
+		$count = 0;
+		//for each geometry (NoGo polygon):
+		for ($x = 0; $x < sizeof($geoms); $x++){
+			//remove the leaflet id from the array
+			unset($geoms[$x]["id"]);
+			//for every geometry except the first one
+			if ($count > 0){
+				//prepend a comma separator to separate the features
+				$FeatureCollection.= ", ";
+			}
+			//add the geometry to the feature collection
+			$FeatureCollection.= json_encode($geoms[$x]);
+			$count += 1;
+		}
+
+		//sql parameter containing the geom
+	    $jsonParam = "
+	        {
+	            \"type\": \"FeatureCollection\",
+	            \"crs\": { \"type\": \"name\", \"properties\": { \"name\": \"urn:ogc:def:crs:OGC:1.3:CRS84\" } },
+	            \"features\": [".$FeatureCollection."]                          
+	        }
+	        ";
+
+		//sql function calling the pgr_nogo_trspViaVertices function from the DB
+		$sql = '
+		SELECT json_build_object(
+	    	\'type\',		\'FeatureCollection\',
+	    	\'features\', 	jsonb_agg(feature)
+	    )
+	    FROM (
+	    	SELECT json_build_object(
+	    		\'type\',		\'Feature\',
+	    		\'id\', 		gid,
+	    		\'geometry\',	ST_AsGeoJSON(geom)::json,
+	    		\'properties\',	to_jsonb(row) - \'id\' - \'geom\'
+	    ) AS
+	    	feature
+	    FROM
+	    	(
+	    		SELECT
+	    			seq AS gid,
+	    			route_result.id1 AS route_id,
+	    			route_result.id3 AS edge_id,
+	    			ways.name,
+	    			the_geom AS geom
+	    		FROM pgr_nogo_trspViaVertices(
+	    				\'SELECT gid::INTEGER AS id, source::INTEGER, target::INTEGER, cost::FLOAT8, reverse_cost::FLOAT8, the_geom AS geom FROM ways\',
+	    				(
+	                        SELECT
+	                            ST_SetSRID(ST_Union(ST_GeomFromGeoJSON(feat->>\'geometry\')), 4326)
+	                        FROM (
+	                            SELECT json_array_elements($1::json->\'features\'
+	                        ) AS
+	                            feat
+	                    ) AS
+	                        f
+	        ),
+	    				$2, /* PUT NODE IDS HERE IN ORDER */
+	    				TRUE,
+	    				TRUE
+	    			) AS route_result
+	    		INNER JOIN
+	    			ways
+	    		ON
+	    			ways.gid = route_result.id3
+	    	)
+	    row) features;
+
+
 		';
 
-	$result = pg_query( $sql ) or die('Query Failed: ' .pg_last_error());
-	
-	$finalvalue = pg_fetch_result($result, 0, 0);
-    echo $finalvalue; //send result back to web page*/
-	
+		// send the query and necessary params to the DB
+		$result = pg_query_params( $conn, $sql, array($jsonParam,to_pg_array($nodes)) ) or die('Query Failed: ' .pg_last_error());
+
+		$finalvalue = pg_fetch_result($result, 0, 0);
+	    echo $finalvalue; //send result back to web page*/
+	}
 
 ?>
--- www/php/functions.php
+++ www/php/functions.php
@@ -67,5 +67,21 @@
 			unlink($file); //delete file
 		}
 	}
+
+	function to_pg_array($set) {
+        settype($set, 'array'); // can be called with a scalar or array
+        $result = array();
+        foreach ($set as $t) {
+            if (is_array($t)) {
+                $result[] = to_pg_array($t);
+            } else {
+                $t = str_replace('"', '\\"', $t); // escape double quote
+                if (! is_numeric($t)) // quote only non-numeric values
+                    $t = '"' . $t . '"';
+                $result[] = $t;
+            }
+        }
+        return '{' . implode(",", $result) . '}'; // format
+    }
 	
 ?>
